50ac6168082fb86832c689ba3d7eb6bcfdc5682c
mioty <hei.mioty.2@gmail.com>
2025-09-03 12:15:26 +0300

    feat: add request get ping

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from importlib.resources import contents
+from pydoc import plaintext
+
 from fastapi import FastAPI;
+from starlette.responses import Response
 
 app = FastAPI()
 
 @app.get(" /ping")
-async def ping():
-    return {"message":"pong"}
+def ping():
+    return Response(content = "pong", status_code = 200, media_type = "text-plain")
+
+
+
+
 
